eede24325fc6f9f75e34878176973efb0d89e8f7
Feefa <rob@buskom.co.uk>
2017-06-21 22:31:05 +0100

    Added Thingy.Diagnostics project

---
+++ Thingy.Diagnostics.Api/DiagnosticLevels.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thingy.Diagnostics.Api
+{
+    public enum DiagnosticLevels
+    {
+        Information,
+        Warning,
+        Error
+    }
+}
---
+++ Thingy.Diagnostics.Api/IDiagnosticConsole.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thingy.Diagnostics.Api
+{
+    public interface IDiagnosticConsole
+    {
+        /// <summary>
+        /// Write a diagnostic message
+        /// </summary>
+        /// <param name="source">The source object</param>
+        /// <param name="method">The name of the method</param>
+        /// <param name="level">The diagnostic level</param>
+        /// <param name="message">The message</param>
+        void WriteMessage(object source, string method, DiagnosticLevels level, string message);
+    }
+}

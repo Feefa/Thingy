9ce8e46565381da575475cb792422f7247ea4467
Feefa J <rob@buskom.co.uk>
2017-07-06 09:43:06 +0100

    Catch internal errors when streaming file to client Also added request details to error pages rather than leaving a comment about avoiding the pitfall of a null request in future

--- Thingy.WebServerLite/WebServerResponse.cs
+++ Thingy.WebServerLite/WebServerResponse.cs
@@ -27,24 +27,31 @@ namespace Thingy.WebServerLite
             HttpListenerResponse.ContentType = mimeTypeProvider.GetMimeType(filePath);
             HttpListenerResponse.StatusCode = 200;
 
-            using (Stream outputStream = HttpListenerResponse.OutputStream)
+            try
             {
-                fileIoMutex.WaitOne();
-
-                try
+                using (Stream outputStream = HttpListenerResponse.OutputStream)
                 {
-                    using (FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read))
+                    fileIoMutex.WaitOne();
+
+                    try
                     {
-                        fileStream.CopyTo(HttpListenerResponse.OutputStream);
-                        fileStream.Close();
+                        using (FileStream fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read, FileShare.Read))
+                        {
+                            fileStream.CopyTo(HttpListenerResponse.OutputStream);
+                            fileStream.Close();
+                        }
                     }
-                }
-                finally
-                {
-                    fileIoMutex.ReleaseMutex();
-                }
+                    finally
+                    {
+                        fileIoMutex.ReleaseMutex();
+                    }
 
-                outputStream.Close();
+                    outputStream.Close();
+                }
+            }
+            catch(Exception exception)
+            {
+                InternalError(null, exception);
             }
         }
 
@@ -64,9 +71,11 @@ namespace Thingy.WebServerLite
 
         public void InternalError(IWebServerRequest request, Exception e)
         {
-            FromString(new MarkUpBuilder()
+            FromString(
+                AddRequestDetails(new MarkUpBuilder()
                 .Append("<!DOCTYPE html>", "<html>", "<head>", "<title>500 Error</title>", "</head>")
                 .Append("<body style=\"font-family: calibri, ariel;\">", "<h1 style=\"background-color: #ffffcf; border : 1px solid black; padding : 8px\">500 - Internal Server Error</h1>")
+                , request)
                 .Append("<div style=\"font-family: consolas, courier; border : 1px solid black; padding : 8px\">")
                 .Append(e.ToString().Replace("\n", "<br/>"))
                 .Append("</div></body>", "</html>")
@@ -75,9 +84,11 @@ namespace Thingy.WebServerLite
 
         public void NotAllowed(IWebServerRequest request)
         {
-            FromString(new MarkUpBuilder()
+            FromString(
+                AddRequestDetails(new MarkUpBuilder()
                 .Append("<!DOCTYPE html>", "<html>", "<head>", "<title>403 Forbidden</title>", "</head>")
                 .Append("<body style=\"font-family: calibri, ariel;\">", "<h1 style=\"background-color: #ffffcf; border : 1px solid black; padding : 8px\">500 - Forbidden</h1>")
+                , request)
                 .Append("<div style=\"font-family: consolas, courier; border : 1px solid black; padding : 8px\">")
                 .Append("The request was valid, but the server is refusing action.<br/>The user might not have the necessary permissions for a resource, or may need an account of some sort.")
                 .Append("</div></body>", "</html>")
@@ -86,13 +97,26 @@ namespace Thingy.WebServerLite
 
         public void NotFound(IWebServerRequest request)
         {
-            FromString(new MarkUpBuilder()
+            FromString(
+                AddRequestDetails(new MarkUpBuilder()
                 .Append("<!DOCTYPE html>", "<html>", "<head>", "<title>404 Not Found</title>", "</head>")
                 .Append("<body style=\"font-family: calibri, ariel;\">", "<h1 style=\"background-color: #ffffcf; border : 1px solid black; padding : 8px\">404 - Not Found</h1>")
+                , request)
                 .Append("<div style=\"font-family: consolas, courier; border : 1px solid black; padding : 8px\">")
                 .Append("The requested resource could not be found but may be available in the future.<br/>Subsequent requests by the client are permissible.")
                 .Append("</div></body>", "</html>")
                 .ToString(), "text/html", 404);
+        }
+
+        private MarkUpBuilder AddRequestDetails(MarkUpBuilder builder, IWebServerRequest request)
+        {
+            return request == null ? builder : builder
+                .Append("<div style=\"font-family: consolas, courier; border : 1px solid black; padding : 8px; margin-bottom : 4px;\">")
+                .Append("<table><tbody>")
+                .Append("<tr><th style=\"padding-right : 4px; text-align : right\">HTTP Method</th><td>", request.HttpMethod, "</td></tr>")
+                .Append("<tr><th style=\"padding-right : 4px; text-align : right\">Url</th><td>", request.HttpListenerRequest.Url, "</td></tr>")
+                .Append("</tbody></table>")
+                .Append("</div>");
         }
 
         public HttpListenerResponse HttpListenerResponse { get; private set; }
